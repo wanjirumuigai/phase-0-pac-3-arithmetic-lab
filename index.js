d7bae098ce9f26aeeea00e2dc0edeb249ac661f6
Pauline Muigai <wanjirumuigai@gmail.com>
2022-11-19 13:06:46 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+function add(a,b) {
+    return a+b;
+}
+function subtract(x,y) {
+    return x-y;
+}
+function multiply(a,b) {
+    return a * b;
+}
+function divide(a,b) {
+    return a/b;
+}
+function increment(n) {
+    
+    return n=n+1;
+}
+function decrement(n) {
+    
+    return n=n-1;
+}
+function makeInt(n) {
+   return parseInt(n,10);
+}
+function preserveDecimal(n) {
+    
+    return parseFloat("2.222");
+  
+    }
+
+
---
+++ test2.js
@@ -0,0 +1,7 @@
+function preserveDecimal(n) {
+    n = [parseFloat("2.222"), parseFloat('n')];
+    return n;
+  
+
+    }
+console.log(preserveDecimal());
